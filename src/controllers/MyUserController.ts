73af46fbea37103d351886f37e5e983631c941ae
Neeraj Singh <869neerajsingh@gmail.com>
2024-09-22 17:20:39 +0530

    feat: add route and controller to update user details

--- src/controllers/MyUserController.ts
+++ src/controllers/MyUserController.ts
@@ -12,7 +12,7 @@ const createCurrentUser = async (req: Request, res: Response) => {
       return res.status(200).json({ message: "User already exists." });
     }
 
-    // if user doesn't exist
+    // if user doesn't exist, create user
     const newUser = new User(req.body);
     await newUser.save();
 
@@ -24,6 +24,35 @@ const createCurrentUser = async (req: Request, res: Response) => {
   }
 };
 
+const updateCurrentUser = async (req: Request, res: Response) => {
+  try {
+    // get the necessary details from request object
+    const {name, addressLine1, country, city} = req.body;
+
+    // fetch user from database on the basis of userId
+    const user = await User.findById(req.userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found." });
+    }
+
+    // update & save user details
+    user.name = name;
+    user.addressLine1 = addressLine1;
+    user.country = country;
+    user.city = city;
+
+    await user.save();
+
+    res.status(200).send(user);
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({ message: "Error updating user." });
+  }
+};
+
 export default {
   createCurrentUser,
+  updateCurrentUser,
 };
--- src/routes/MyUserRoute.ts
+++ src/routes/MyUserRoute.ts
@@ -1,9 +1,11 @@
 import express from "express";
 
 import MyUserController from "../controllers/MyUserController";
+import { jwtCheck, jwtParse } from "../middlewares/auth";
 
 const router = express.Router();
 
-router.post("/", MyUserController.createCurrentUser);
+router.post("/", jwtCheck, MyUserController.createCurrentUser);
+router.put("/", jwtCheck, jwtParse, MyUserController.updateCurrentUser);
 
 export default router;
