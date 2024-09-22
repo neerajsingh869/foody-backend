cda9c08136b2a9987995307cc2fb59eb37573ed3
Neeraj Singh <869neerajsingh@gmail.com>
2024-09-22 17:18:39 +0530

    feat: create another middleware to extract user id and auth0 id from access token

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,4 +1,18 @@
+import { NextFunction, Request, Response } from "express";
 import { auth } from "express-oauth2-jwt-bearer";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import User from "../models/User";
+
+/* adding auth0Id and userId as 
+new property in Request interface */
+declare global {
+  namespace Express {
+    interface Request {
+      auth0Id: string,
+      userId: string,
+    }
+  }
+}
 
 // middleware to check if access token is valid
 export const jwtCheck = auth({
@@ -6,3 +20,33 @@ export const jwtCheck = auth({
   issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
   tokenSigningAlg: process.env.AUTH0_TOKEN_SIGNING_ALGORITHM,
 });
+
+/* middleware to decode access token, find auth0Id & userId, 
+and then update the req object before sending the request to controller */
+export const jwtParse = async (req: Request, res: Response, next: NextFunction) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith("Bearer ")) {
+    return res.sendStatus(401);
+  }
+
+  const accessToken = authorization.split(" ")[1];
+
+  try {
+    const decodedAccessToken = jwt.decode(accessToken) as jwt.JwtPayload;
+
+    const auth0Id = decodedAccessToken.sub;
+    const user = await User.findOne({auth0Id});
+    
+    if (!user) {
+      return res.sendStatus(401);
+    }
+
+    req.auth0Id = auth0Id as string;
+    req.userId = user._id.toString();
+
+    next();
+  } catch (error) {
+    res.sendStatus(401);
+  }
+};
