02e5e8ecb419c495020b8a75a28b72ac05952faa
Neeraj Singh <869neerajsingh@gmail.com>
2024-09-22 17:20:10 +0530

    refactor: remove jwtCheck middleware from index.ts and put it in MyUserRoute

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import "dotenv/config";
 import mongoose from "mongoose";
 
 import MyUserRoute from "./routes/MyUserRoute";
-import { jwtCheck } from "./middlewares/auth";
 
 // establish connection between backend and mongodb
 mongoose
@@ -21,7 +20,7 @@ app.use(express.json());
 resource sharing */
 app.use(cors());
 
-app.use("/api/my/user", jwtCheck, MyUserRoute);
+app.use("/api/my/user", MyUserRoute);
 
 /* start express server and listen
 for incoming requests */
